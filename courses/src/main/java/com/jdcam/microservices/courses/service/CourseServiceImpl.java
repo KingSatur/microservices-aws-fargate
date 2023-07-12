03c9a281b55de2b5006d9704430849731d386785
Juan Campillo <juancawool@outlook.com>
2023-07-12 11:51:03 -0500

    feat: creating course microservice and adding postman collection to consume both services

---
+++ courses/src/main/java/com/jdcam/microservices/courses/service/CourseServiceImpl.java
@@ -0,0 +1,52 @@
+package com.jdcam.microservices.courses.service;
+
+import com.jdcam.microservices.courses.entity.Course;
+import com.jdcam.microservices.courses.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseServiceImpl implements CourseService{
+
+    private final CourseRepository courseRepository;
+
+    public CourseServiceImpl(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Course> getCourses() {
+        return (List<Course>) this.courseRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Course> getCourseById(Long id) {
+        return this.courseRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Optional<Course> update(Course course, Long id) {
+        return this.getCourseById(id).map(courseFromDb -> {
+            courseFromDb.setName(course.getName());
+            return Optional.of(this.courseRepository.save(courseFromDb));
+        }).orElse(Optional.empty());
+    }
+
+    @Override
+    @Transactional
+    public Course save(Course course) {
+        return this.courseRepository.save(course);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        this.courseRepository.deleteById(id);
+    }
+}
---
+++ courses/src/test/java/com/jdcam/microservices/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdcam.microservices.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
