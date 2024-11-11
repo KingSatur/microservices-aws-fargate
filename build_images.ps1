49bc84f8376fe3f6f701b84cbbd3b50678e8bb06
Code with Juanda <codwithjuanda@gmail.com>
2024-11-11 12:34:36 -0500

    feat: initial config for auth-service

--- build_images.ps1
+++ build_images.ps1
@@ -10,7 +10,9 @@ if (-not $version) {
 docker build -t davidlearner/users-service -f .\users\Dockerfile .\users\
 docker build -t davidlearner/courses-service -f .\users\Dockerfile .\courses\
 docker build -t davidlearner/api-gateway-service -f .\api_gateway\Dockerfile .\api_gateway\
+docker build -t davidlearner/auth-service -f .\auth\Dockerfile .\auth\
 
 docker push davidlearner/users-service
 docker push davidlearner/courses-service
 docker push davidlearner/api-gateway-service
+docker push davidlearner/auth-service
