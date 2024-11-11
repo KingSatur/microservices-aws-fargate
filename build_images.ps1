288212985764f5724b4d75fecf0f77800ffe7748
Code with Juanda <codwithjuanda@gmail.com>
2024-11-11 11:36:29 -0500

    feat: update configs for api gateway and change svc type to ClusterIP for users and courses service in order to restrict consume only through api gateway

--- build_images.ps1
+++ build_images.ps1
@@ -7,8 +7,10 @@ if (-not $version) {
     exit 1
 }
 
-docker build -t davidlearner/users:$version -f .\users\Dockerfile .\users\
-docker build -t davidlearner/courses:$version -f .\users\Dockerfile .\courses\
+docker build -t davidlearner/users-service -f .\users\Dockerfile .\users\
+docker build -t davidlearner/courses-service -f .\users\Dockerfile .\courses\
+docker build -t davidlearner/api-gateway-service -f .\api_gateway\Dockerfile .\api_gateway\
 
-docker push davidlearner/users:$version
-docker push davidlearner/courses:$version
+docker push davidlearner/users-service
+docker push davidlearner/courses-service
+docker push davidlearner/api-gateway-service
